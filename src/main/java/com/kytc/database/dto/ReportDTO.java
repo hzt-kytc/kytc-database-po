1c36a403325ec61d5a0aa3cc9968fef6af8cd8da
fish <fish@51biaoqing.com>
2018-06-05 11:50:28 +0800

    报表及忽略文件...

--- src/main/java/com/kytc/database/dto/ReportDTO.java
+++ src/main/java/com/kytc/database/dto/ReportDTO.java
@@ -3,7 +3,11 @@ package com.kytc.database.dto;
 public class ReportDTO {
 	private String category;
 	private String title;
+	private String subTitle;
 	private Double value;
+	private String xTitle;
+	private String yTitle;
+	private String xList;
 	public String getCategory() {
 		return category;
 	}
@@ -22,6 +26,30 @@ public class ReportDTO {
 	public void setValue(Double value) {
 		this.value = value;
 	}
+	public String getSubTitle() {
+		return subTitle;
+	}
+	public void setSubTitle(String subTitle) {
+		this.subTitle = subTitle;
+	}
+	public String getxTitle() {
+		return xTitle;
+	}
+	public void setxTitle(String xTitle) {
+		this.xTitle = xTitle;
+	}
+	public String getyTitle() {
+		return yTitle;
+	}
+	public void setyTitle(String yTitle) {
+		this.yTitle = yTitle;
+	}
+	public String getxList() {
+		return xList;
+	}
+	public void setxList(String xList) {
+		this.xList = xList;
+	}
 	@Override
 	public String toString() {
 		StringBuilder builder = new StringBuilder();
@@ -29,8 +57,16 @@ public class ReportDTO {
 		builder.append(category);
 		builder.append(", title=");
 		builder.append(title);
+		builder.append(", subTitle=");
+		builder.append(subTitle);
 		builder.append(", value=");
 		builder.append(value);
+		builder.append(", xTitle=");
+		builder.append(xTitle);
+		builder.append(", yTitle=");
+		builder.append(yTitle);
+		builder.append(", xList=");
+		builder.append(xList);
 		builder.append("]");
 		return builder.toString();
 	}
